2cff380bf12aeaa8aa4d6fd9d9b43d4d910c35d7
s.ebrahimzadeh <s.ebrahimzadeh@zi-tel.com>
2025-04-29 17:02:20 +0330

    worked of delete contact

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,7 +1,8 @@
-import { useContactsListQuery } from "../hooks";
+import { useContactsListQuery, useDeleteContactMutation } from "../hooks";
 
 export const ContactList = () => {
     const {data, isLoading, error} = useContactsListQuery();
+    const { mutate: deleteContact, isPending} = useDeleteContactMutation();
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error loading contacts</p>;
@@ -11,6 +12,13 @@ export const ContactList = () => {
             {data?.map((item) => (
                 <li key={item.id}>
                     <strong>{item.name}</strong> - {item.phone}
+                    <button
+                        onClick={() => deleteContact(item.id)}
+                        disabled={isPending}
+                        className="text-red-500 hover:underline"
+                    >
+                        {isPending ? "Deleting..." : "Delete"}
+                    </button>
                 </li>
             ))}
         </ul>
